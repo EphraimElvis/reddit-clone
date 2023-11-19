820ce0e685215fed13eb65d970e863891444991f
Ephraim Nyakpo Elvis <ephraim.elvis@yahoo.co.uk>
2023-11-19 19:59:56 +0000

    User controller created

---
+++ backend/controllers/user.js
@@ -0,0 +1,24 @@
+const User = require("../models/User");
+//const Post = require("../models/Post");
+const bcrypt = require("bcrypt");
+
+exports.signup = (req, res) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    User.create({
+      email: req.body.email,
+      password: hash,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    })
+      .then(() => {
+        res.status(201).json({
+          message: "User added successfully!",
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          error: error,
+        });
+      });
+  });
+};
