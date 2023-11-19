e5b29ae7b5064e9d79852020bad1482267082e8f
Ephraim Nyakpo Elvis <ephraim.elvis@yahoo.co.uk>
2023-11-19 12:43:29 +0000

    Capitalized user model filename

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,23 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+
+const User = sequelize.define("user", {
+  email: {
     type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  firsstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+exports.module = User;
