7aa40830be2408a45f5d201861fed9ed8ade12e4
Ephraim Nyakpo Elvis <ephraim.elvis@yahoo.co.uk>
2023-11-25 07:17:05 +0000

    multer config created

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 require("./database");
 const express = require("express");
+const path = require("path");
 
 const app = express();
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
