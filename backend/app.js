783dd212968bb95bc1cab9ab3c711364e1eb6e89
Ephraim Nyakpo Elvis <ephraim.elvis@yahoo.co.uk>
2023-12-02 21:06:23 +0000

    App routes created

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,21 @@ const path = require("path");
 const app = express();
 const userRoutes = require("./routes/user");
 const userPostRoutes = require("./routes/post");
+const getAllPostRoutes = require("./routes/post");
+const deleteAllPostRoutes = require("./routes/post");
+const deletePostRoutes = require("./routes/post");
+const loginRoutes = require("./routes/user");
+const hasReadRoutes = require("./routes/post");
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/api/", userRoutes);
+app.use("/api/", loginRoutes);
 app.use("/api/", userPostRoutes);
+app.use("/api/", deletePostRoutes);
+app.use("/api/", getAllPostRoutes);
+app.use("/app", deleteAllPostRoutes);
 
 module.exports = app;
