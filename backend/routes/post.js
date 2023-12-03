c276d3d0d492d065bc84a5a2005cf08cf992c216
Ephraim Nyakpo Elvis <ephraim.elvis@yahoo.co.uk>
2023-12-03 18:37:35 +0000

    auth implemented

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const multer = require("../middleware/multer-config");
-const postController = require("../controllers/post");
+const postCtrl = require("../controllers/post");
+const auth = require("../middleware/auth");
 
-router.post("/userpost", multer, postController.userpost);
+router.post("/posts", auth, multer, postCtrl.create);
+router.get("/posts", auth, postCtrl.getAll);
+router.delete("/posts/:id", auth, postCtrl.deletePost);
+router.delete("/posts", auth, postCtrl.deleteAll);
+router.get("/posts/:postId/:userId", auth, postCtrl.hasRead);
+router.post("/posts/:postId/:userId", auth, postCtrl.readPost);
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user");
+const auth = require("../middleware/auth");
 
 router.post("/signup", userController.signup);
 
