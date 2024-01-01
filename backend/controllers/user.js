12b5095dfe305704ab1be13ce4a735fcd3605e26
Ephraim Nyakpo Elvis <ephraim.elvis@yahoo.co.uk>
2024-01-01 23:17:10 +0000

    delete and view user profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
-const { User } = require("../models/index");
+const { User, Post } = require("../models/index");
+
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
@@ -56,4 +57,33 @@ exports.login = (req, res) => {
     .catch((error) => {
       res.status(500).json(error);
     });
+};
+
+exports.profile = (req, res) => {
+  const userId = parseInt(res.req.userId);
+  //console.log("hello world", res.req.userId);
+  User.findByPk(userId)
+    .then((data) => {
+      if (!data) {
+        return res.sendStatus(404);
+      }
+
+      res.status(200).json({ data });
+    })
+    .catch((error) => error);
+};
+
+exports.deleteProfile = (req, res) => {
+  const userId = parseInt(res.req.userId);
+
+  User.destroy({
+    where: {
+      id: userId,
+    },
+    force: true,
+  })
+    .then(() => {
+      res.status(200).json({ message: "User account deleted" });
+    })
+    .catch((error) => res.status(500).json(error));
 };
